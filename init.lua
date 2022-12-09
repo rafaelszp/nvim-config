3cd24e9b463ab1d8f3a2528b0365366bd3240a5e
Rafael Pereira <rafaelsouza@sistemafieg.org.br>
2022-12-09 10:41:52 -0300

    Initial config

---
+++ init.lua
@@ -0,0 +1,25 @@
+-- Packer installation required: https://github.com/wbthomason/packer.nvim
+-- Install script: git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+require('basics')
+require('coc-config')
+require('colors')
+require('lualine-config')
+require('barbar-config')
+require('telescope-config')
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "all",
+  ignore_install = { "phpdoc" },
+  context_commentstring = {
+    enable = true
+  },
+  highlight = {
+    enable = true,
+    disable = { "lua" }
+  },
+  indent = {
+    enable = true
+  }
+}
+
+require('plugins')
---
+++ lua/plugins.lua
@@ -0,0 +1,44 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  use {'neoclide/coc.nvim', branch = 'release'}
+  use 'artanikin/vim-synthwave84'
+  use 'nvim-treesitter/nvim-treesitter' 
+  use 'tpope/vim-commentary'
+  use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use 'lukas-reineke/indent-blankline.nvim'
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { "nvim-telescope/telescope-file-browser.nvim" }
+  use {
+    "ur4ltz/surround.nvim",
+    config = function()
+      require"surround".setup {mappings_style = "surround"}
+    end
+  }
+  use {
+    'nvim-lualine/lualine.nvim',
+--    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use 'nvim-tree/nvim-web-devicons'
+  use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
+
+  use 'preservim/nerdtree'
+  use {
+		'phaazon/hop.nvim',
+		branch = 'v2', -- optional but strongly recommended
+		config = function()
+			-- you can configure Hop the way you like here; see :h hop-config
+			require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+		end
+	}
+
+  use 'fedepujol/move.nvim'
+
+  -- You add plugins here  
+end)
+

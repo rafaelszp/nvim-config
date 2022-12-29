7ebf7654b1d693512a231c9c307b6997f6468618
rafaelsouza@fieg.com.br <rafaelsouza@fieg.com.br>
2022-12-29 10:47:32 -0300

    definition shortcuts

--- lua/coc-config.lua
+++ lua/coc-config.lua
@@ -8,6 +8,11 @@ vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr
 vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
 vim.api.nvim_set_keymap("i", "<S-TAB>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
+
+vim.api.nvim_set_keymap("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", {silent = true})
+
 vim.o.hidden = true
 vim.o.backup = false
 vim.o.writebackup = false
